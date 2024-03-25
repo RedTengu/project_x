478d76e68e13755e92c44fdbb9cbd21eb534234c
Red Yakuza <muratborokov@gmail.com>
2024-03-25 11:04:39 +0300

    refactor: вынести роуты и настроить отдельный конфиг

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,7 @@
-import { Suspense } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-
-import { MainPage } from "pages/MainPage";
-import { AboutPage } from "pages/AboutPage";
+import { AppRouter } from "app/providers/router";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
@@ -14,12 +11,7 @@ const App = () => {
       <button onClick={toggleTheme}>Сменить тему</button>
       <Link to='/'>Главная</Link>
       <Link to='/about'>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   )
 };
